d79f9a5b18783bcb3b47ad461a81fc2f08dac350
aaronmt1011 <aaronmt1011@gmail.com>
2023-01-18 16:55:05 -0700

    Finished finale part of exercise

--- javascript.js
+++ javascript.js
@@ -14,3 +14,17 @@ const dirtyDan = document.createElement('h3');
 dirtyDan.style.cssText = 'color: blue;';
 dirtyDan.textContent = "What makes you think you can be Dirty Dan?";
 container.appendChild(dirtyDan);
+
+const dirtyDiv = document.createElement('div');
+dirtyDiv. setAttribute('style', 'border: black; background: pink;');
+
+let dirtyH2 = document.createElement('h2');
+dirtyH2.textContent = "I'm Dirty.";
+
+let dirtyh1 = document.createElement('h1');
+dirtyh1.textContent = "I'd say I'm Dirty Dan!";
+
+dirtyDiv.appendChild(dirtyH2);
+dirtyDiv.appendChild(dirtyh1);
+
+container.appendChild(dirtyDiv);
