8c3b997c342d77eb36c9e65881421b3a23ec7683
aaronmt1011 <aaronmt1011@gmail.com>
2023-01-18 16:26:46 -0700

    Finished part 1 and 2 of exercise"

--- javascript.js
+++ javascript.js
@@ -3,3 +3,14 @@ const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'All right, Pinhead. Your time is up!'
+container.appendChild(content);
+
+const pinhead = document.createElement('p');
+pinhead.style.cssText = 'color: red;';
+pinhead.textContent = "Who you callin' Pinhead? I wanna be Dirty Dan!";
+container.appendChild(pinhead);
+
+const dirtyDan = document.createElement('h3');
+dirtyDan.style.cssText = 'color: blue;';
+dirtyDan.textContent = "What makes you think you can be Dirty Dan?";
+container.appendChild(dirtyDan);
